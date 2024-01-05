21ec9ca99cdcaa58be6c1714e86a50d4c0a7c0f8
PTHARRISH <harrishpt@gmail.com>
2024-01-06 00:33:09 +0530

    cors origin changes

--- Project/settings.py
+++ Project/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS: [
-    "http://127.0.0.1:8000/",
-    "http://localhost:3000/"
+    # "http://127.0.0.1:8000/",
+    "http://localhost:3000"
 ]
 
 
